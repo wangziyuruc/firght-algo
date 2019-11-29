8d30d3679ddb2dd33db959a2cc3dfb13deb4e5e2
wzy452595356 <52zhoulu>
2019-11-29 22:08:09 +0800

    first commit

---
+++ array/src/ArrayClass1.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayClass1 {
+    public static void main(String[] args) {
+        List<String> str = new ArrayList<>();
+        System.out.println("2222");
+    }
+}
